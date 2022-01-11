b80448ec9b2b01c76bb214f60c8f50307a9bb78a
Victor Almeida <victorh.almeida7@gmail.com>
2022-01-11 13:45:12 -0300

    novo SellerFormController e SellerForm.fxml

---
+++ src/gui/SellerFormController.java
@@ -0,0 +1,142 @@
+package gui;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import db.DbException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.entities.Seller;
+import model.exceptions.ValidationException;
+import model.services.SellerService;
+
+public class SellerFormController implements Initializable{
+	private Seller entity;
+	private SellerService service;
+	
+	// Lista de objetos que irão receber o metodo do serviço de evento
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+	
+	@FXML
+	private TextField txtId;
+	
+	@FXML
+	private TextField txtName;
+	
+	@FXML
+	private Label labelErrorName;
+	
+	@FXML
+	private Button btSave;
+	
+	@FXML
+	private Button btCancel;
+	
+	public void setSeller(Seller entity) {
+		this.entity = entity;
+	}
+	
+	public void setSellerService(SellerService service) {
+		this.service = service;
+	}
+	
+	// Metodo para inscrever obj na lista de objts que irão receber o metodo do serviço de evento
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+	}
+	
+	@FXML
+	public void onBtSaveAction(ActionEvent event) {
+		// Verificação pq estamos fazendo uma injerção de dependencia manual e não com um framework
+		if(entity == null)
+			throw new IllegalStateException("Entity was null");
+		if(service == null)
+			throw new IllegalStateException("Service was null");
+		
+		try {
+			entity = getDadosForm();
+			service.saveOrUpdate(entity);
+			
+			notifyDataChangeListeners();
+			
+			// para fechar a janela
+			// pegar referencia para a janela atual
+			Utils.palcoAtual(event).close();
+		}catch(ValidationException e) {
+			setErrorMessages(e.getErrors());
+		}catch(DbException e) {
+			Alerts.mostrarAlerta("Erro ao salavar objeto", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+	private void notifyDataChangeListeners() {
+		for(DataChangeListener listener : dataChangeListeners) {
+			listener.onDadosAlterados();
+		}
+		
+	}
+
+	private Seller getDadosForm() {
+		Seller obj = new Seller();
+		
+		//Exceção que será lançada quando ocorrer erro citado na verificação
+		ValidationException exception = new ValidationException("Validation error");
+		
+		obj.setId(Utils.converterParaInt(txtId.getText()));
+		
+		//obj a ser verificado "txtName"
+		if(txtName.getText() == null || txtName.getText().trim().equals(""))
+			exception.addError("name", "Field can't be empty");
+		
+		obj.setName(txtName.getText());
+		
+		// Testar se a coleção de erros da Classe "ValidationException" tem pelo menos 1 erro
+		if(exception.getErrors().size() > 0)
+			// caso a condição for verdadeira, lançará a exceção seguinte
+			throw exception;
+		
+		return obj;
+	}
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.palcoAtual(event).close();
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		Constraints.apenasNumInt(txtId);
+		Constraints.tamanhoMaximo(txtName, 30);
+		
+	}
+	
+	public void atualizarDadosDoForm() {
+		if(entity == null)
+			throw new IllegalStateException("Entity was null");
+		
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(entity.getName());
+	}
+	
+	// Metodo que prenche a mesnagem de erro no Label vazio da janela de "SellerForm"
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		
+		if(fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));
+		}
+	}
+
+}
--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
@@ -16,7 +17,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -24,6 +27,8 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
 import model.services.SellerService;
@@ -109,36 +114,36 @@ public class SellerListController implements Initializable, DataChangeListener{
 	
 	// -Como parametro, uma referencia para o Stage da janela que criou a janela de dialogo
 	private void criarFormulario(Seller obj, String absoluteName, Stage parentStage) {
-//		try {
-//			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-//			Pane pane = loader.load();
-//			
-//			SellerFormController controller = loader.getController();
-//			controller.setSeller(obj);
-//			controller.setSellerService(new SellerService());
-//			controller.subscribeDataChangeListener(this);
-//			controller.updateFormData();
-//			
-//			// criar uma outra janela
-//			Stage dialogStage = new Stage();
-//			dialogStage.setScene(new Scene(pane));
-//			
-//			// Impedir a tela de ser Rendimencionada "false"
-//			dialogStage.setResizable(false);
-//			
-//			// Indicar quem é o Stage pai dessa janela
-//			dialogStage.initOwner(parentStage);
-//			
-//			// Dizer se a janela vai ser modal ou vai ter outro comportamento
-//			/* ...Modality.WINDOW_MODAL... mantém a tela travada.
-//			 *  enquanto não for fechada, não poderá acessar a janela anterior/ que está atrás*/
-//			dialogStage.initModality(Modality.WINDOW_MODAL);
-//			dialogStage.showAndWait();
-//			
-//		}catch(IOException e) {
-//			Alerts.mostrarAlerta("IO Exception", "Erro ao Carregar tela!", e.getMessage(), AlertType.ERROR);
-//			//e.printStackTrace();
-//		}
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			
+			SellerFormController controller = loader.getController();
+			controller.setSeller(obj);
+			controller.setSellerService(new SellerService());
+			controller.subscribeDataChangeListener(this);
+			controller.atualizarDadosDoForm();
+			
+			// criar uma outra janela
+			Stage dialogStage = new Stage();
+			dialogStage.setScene(new Scene(pane));
+			
+			// Impedir a tela de ser Rendimencionada "false"
+			dialogStage.setResizable(false);
+			
+			// Indicar quem é o Stage pai dessa janela
+			dialogStage.initOwner(parentStage);
+			
+			// Dizer se a janela vai ser modal ou vai ter outro comportamento
+			/* ...Modality.WINDOW_MODAL... mantém a tela travada.
+			 *  enquanto não for fechada, não poderá acessar a janela anterior/ que está atrás*/
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+			
+		}catch(IOException e) {
+			Alerts.mostrarAlerta("IO Exception", "Erro ao Carregar tela!", e.getMessage(), AlertType.ERROR);
+			//e.printStackTrace();
+		}
 	}
 
 	@Override
